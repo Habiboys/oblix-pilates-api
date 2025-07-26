7d2fa192e9b81c6c2f45c5f2934ea016993de7d7
Habiboys <nouvalhabibie18@gmail.com>
2025-07-26 19:48:34 +0700

    member package

--- src/index.js
+++ src/index.js
@@ -39,13 +39,19 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
 }));
 
 app.use(express.json());
+// app.use(cors({
+//     origin: '*',
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+// }));
 app.use(cors({
-    origin: '*',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: [
+    'https://oblix-pilates.vercel.app',
+    'http://localhost:5173' // untuk development
+  ],
+  credentials: true
 }));
-
 // Serve static files dari folder uploads
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
