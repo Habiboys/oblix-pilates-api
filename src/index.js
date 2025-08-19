2c0c4237ad36c67607d58efa90a8f5a28fc92020
Habiboys <nouvalhabibie18@gmail.com>
2025-08-19 15:36:20 +0700

    cors

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ app.use(cors({
     'https://www.oblixpilates.id'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  // allowedHeaders: ['Content-Type', 'Authorization'],
   // credentials: true
 }));
 
